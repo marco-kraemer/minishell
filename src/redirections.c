acaa248b4c539440dd186a29131690e031634f05
marcokraemer <marco.k.santos@example.com>
2021-06-17 10:01:31 -0300

    Update

--- src/redirections.c
+++ src/redirections.c
@@ -6,19 +6,30 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 14:57:44 by maraurel          #+#    #+#             */
-/*   Updated: 2021/06/17 09:34:53 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/06/17 10:00:23 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 
-void	redirect_input(char *filename, char *data)
+void	redirect_output(char *filename, char *data)
 {
 	int	fd;
 
 	fd = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 0777);
-	write (fd, data, ft_strlen(data));
+	if (data != NULL)
+		write (fd, data, ft_strlen(data));
+	close(fd);
+}
+
+void	append_output(char *filename, char *data)
+{
+	int	fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0777);
+	if (data != NULL)
+		write (fd, data, ft_strlen(data));
 	close(fd);
 }
 
@@ -31,7 +42,12 @@ int	check_redirection(char **args, char *value)
 	{
 		if (ft_strcmp(args[i], ">") == 0)
 		{
-			redirect_input(args[i + 1], value);
+			redirect_output(args[i + 1], value);
+			return (0);
+		}
+		else if (ft_strcmp(args[i], ">>") == 0)
+		{
+			append_output(args[i + 1], value);
 			return (0);
 		}
 		i++;
