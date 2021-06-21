e11455b57e0968fd426f10c4a4332ed463401bd8
marco-kraemer <marco.k.santos@example.com>
2021-06-21 10:47:33 -0300

    Update

--- src/minishell.c
+++ src/minishell.c
@@ -6,24 +6,25 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 11:12:29 by maraurel          #+#    #+#             */
-/*   Updated: 2021/06/18 11:59:49 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/06/21 10:46:34 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	print_prompt()
+char	*readinput()
 {
 	char	cwd[FILENAME_MAX];
+	char	*line;
 	int		i;
 
 	i = 0;
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		strerror(3);
-	write(1, "\033[0;36m", ft_strlen("\033[0;36m")); // CYAN
-	ft_putstr_fd("42@Minishell$ ", 1);
-	write(1, "\033[0m", ft_strlen("\033[0m"));
-	write(1, "$ ", 2);
+	line = readline("\033[0;36m minishell$ \033[0;37m");
+	if (ft_strlen(line) != 0)
+		add_history(line);
+	return (line);
 }
 
 void	sigintHandler(int sig_num)
@@ -31,18 +32,13 @@ void	sigintHandler(int sig_num)
 	if (!sig_num)
 		return;
 	printf("\n");
-	print_prompt();	
+	readinput();
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	**line;
-	char	**commands;
-	char	**args;
+	char	*line;
 	char	**env;
-	char	*value;
-	int		i;
-	int		j;
 
 	env = get_variable_list(envp);
 	if (argc == 54225 && argv)
@@ -50,23 +46,7 @@ int	main(int argc, char **argv, char **envp)
 	while (TRUE)
 	{
 		signal(SIGINT, sigintHandler);
-		print_prompt();
-		line = ft_split(read_line(), ';');
-		i = 0;
-		while (line[i])
-		{
-			j = 0;
-			commands = ft_split(line[i], '|');
-			while (commands[j])
-			{
-				args = ft_split(line[i], ' ');
-				value = execute(args, env, line[i]);
-				j++;
-			}
-			if (value != NULL)
-				printf("%s\n", value);
-			i++;
-		}
+		line = readinput();
 	}
 	exit (EXIT_SUCCESS);
 }
