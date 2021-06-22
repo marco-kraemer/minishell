0016de8ab9803026dff9ea3c2ab8afcaa247e60f
marco-kraemer <marco.k.santos@example.com>
2021-06-21 22:06:49 -0300

    Update

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,67 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 11:12:29 by maraurel          #+#    #+#             */
-/*   Updated: 2021/06/21 14:35:43 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/06/21 21:58:13 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void execArgsPiped(char** parsed, char** parsedpipe)
+{
+    // 0 is read end, 1 is write end
+    int pipefd[2]; 
+    pid_t p1, p2;
+		printf("parsed: %s\n", parsedpipe[0]);
+  
+    if (pipe(pipefd) < 0) {
+        printf("\nPipe could not be initialized");
+        return;
+    }
+    p1 = fork();
+    if (p1 < 0) {
+        printf("\nCould not fork");
+        return;
+    }
+  
+    if (p1 == 0) {
+        // Child 1 executing..
+        // It only needs to write at the write end
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+  
+        if (execvp(parsed[0], parsed) < 0) {
+            printf("\nCould not execute command 1..");
+            exit(0);
+        }
+    } else {
+        // Parent executing
+        p2 = fork();
+  
+        if (p2 < 0) {
+            printf("\nCould not fork");
+            return;
+        }
+  
+        // Child 2 executing..
+        // It only needs to read at the read end
+        if (p2 == 0) {
+            close(pipefd[1]);
+            dup2(pipefd[0], STDIN_FILENO);
+        	close(pipefd[0]);
+            if (execvp(parsedpipe[0], parsedpipe) < 0)
+	    {
+                printf("Could not execute command 2..\n");
+                //exit(0);
+            }
+        } else {
+            // parent executing, waiting for two children
+            wait(NULL);
+            wait(NULL);
+        }
+    }
+}
 
 char	*readinput()
 {
@@ -36,7 +92,7 @@ void	sigintHandler(int sig_num)
 }
 
 // Function where the system command is executed
-void execArgs(char** parsed, char **envp)
+void	launch(char **parsed, char **envp)
 {
 	pid_t pid;
 
@@ -48,7 +104,9 @@ void execArgs(char** parsed, char **envp)
 	}
 	else if (pid == 0) 
 	{
-		if (execve(parsed[0], parsed, envp) < 0)
+		if (ft_strlen(envp[0]) == 3535)
+			printf("oi\n");
+		if (execvp(parsed[0], parsed) < 0)
 			printf("%s: No such file or directory\n", parsed[0]);
 		exit(0);
 	}
@@ -62,9 +120,16 @@ void execArgs(char** parsed, char **envp)
 
 int	checkFlag(char *line)
 {
+	char	**s;
+	int		i;
+	
 	if (ft_strlen(line) == 0)
 		return (0);
-	else if (ft_strncmp(line, "./", 2) == 0)
+	s = ft_split(line, '|');
+	i = 0;
+	while (s[i])
+		i++;
+	if (i > 1)
 		return (2);
 	return (1);
 }
@@ -74,7 +139,11 @@ int	main(int argc, char **argv, char **envp)
 	char	*line;
 	char	**env;
 	char	**parsedArgs;
+	char	**parsedArgsPiped;
+	char	**tmp;
 	int		flag;
+	char	*value;
+	int		i;
 
 	env = get_variable_list(envp);
 	if (argc == 54225 && argv)
@@ -84,10 +153,15 @@ int	main(int argc, char **argv, char **envp)
 	{
 		signal(SIGINT, sigintHandler);
 		line = readinput();
-		flag = checkFlag(line); // 0 if nothing, 1 if command, 2 if binary, 3 if including pipe;
-		parsedArgs = ft_split(line, ' ');
-		if (flag == 2)
-			execArgs(parsedArgs, envp);
+		i = 0;
+		flag = checkFlag(line); // 0 if nothing, 1 if command or binary, 2 if including pipe;
+		tmp = ft_split(line, '|');
+		parsedArgs = ft_split(tmp[0], ' '); // From beginning to '|' or '\0;
+		parsedArgsPiped = ft_split(tmp[1], ' '); // From '|' to end;
+		if (flag == 1)
+			value = execute(parsedArgs, envp, line);
+		else if (flag == 2)
+			execArgsPiped(parsedArgs, parsedArgsPiped);
 	}
 	exit (EXIT_SUCCESS);
 }
--- src/run_shell.c
+++ src/run_shell.c
@@ -6,74 +6,11 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 14:34:07 by maraurel          #+#    #+#             */
-/*   Updated: 2021/06/18 12:00:20 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/06/21 16:47:42 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-char	*read_line(void)
-{
-	int		bufsize;
-	int		position;
-	int		c;
-	char	buf[1];
-	char	*buffer;
-
-	bufsize = BUFF_SIZE;
-	position = 0;
-	buffer = malloc(sizeof(char) * bufsize);
-	if (!buffer)
-	{
-		printf("Allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-	while (TRUE)
-	{
-		c = read(0, buf, 1);
-	//	printf("%c\n", buf[0]);
-		if (buf[0] == '\n')
-		{
-			buffer[position] = '\0';
-			return (buffer);
-		}
-		buffer[position] = buf[0];
-		position++;
-	}
-}
-
-char	*launch_program(char **args)
-{
-	int	pid;
-	int	wpid;
-	int	status;
-
-	status = 0;
-	pid = fork(); // Creating child and parent process
-	//Treat child, replacing it with a new process 
-	if (pid == 0)
-	{
-		if (execvp(args[0], args) == -1) // CHANGE LATER (NOT ALLOWED FUNCTION)
-			printf("shell: No such file or directory\n");
-		exit(EXIT_FAILURE);
-	} 
-	// Check error (fork < 0)
-	else if (pid < 0)
-	{
-		printf("%s: command not found\n", args[0]);
-		strerror(3);
-	}
-	// Treat parent
-	else
-	{
-		wpid = 1;
-		while (wpid >= 0)
-		{
-			wpid = waitpid(pid, &status, WUNTRACED); // WUNTRACED = child processes specified by pid that are stopped
-		}
-	}
-	return (NULL);
-}
 
 char	*execute(char **args, char **envp, char *line)
 {
@@ -97,10 +34,14 @@ char	*execute(char **args, char **envp, char *line)
 	else if (ft_strcmp(args[0], "exit") == 0)
 		free_and_exit(args, line);
 	else if (ft_strncmp(args[0], "./", 2) == 0)
-		return (launch_program(args));
+	{
+		launch(args, envp);
+		return (NULL);
+	}
 	else
 	{
-		printf("%s: command not found\n", args[0]);
+		launch(args, envp);
+		//printf("%s: command not found\n", args[0]);
 		return (NULL);
 	}
 	check_redirection(args, ret);
