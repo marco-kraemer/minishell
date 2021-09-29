9ae7568d78d6d888d42b80d97e0cf603febd7634
marco-kraemer <marco.k.santos@hotmail.com>
2021-09-29 12:04:13 -0300

    Update

--- src/extras/inout.c
+++ src/extras/inout.c
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 09:23:20 by maraurel          #+#    #+#             */
-/*   Updated: 2021/09/29 11:56:19 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/09/29 12:03:50 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,17 +46,12 @@ char	*get_in_out_file(char **args, char *type1, char *type2)
 	filename = NULL;
 	while (args[i])
 	{
-		if (ft_strncmp(args[i], "<<<", 3) == 0 || ft_strncmp(args[i], ">>>", 3) == 0)
-		{
-			printf("minishell: syntax error\n");
-			return (NULL);
-		}
 		if (ft_strcmp(args[i], type1) == 0 || ft_strcmp(args[i], type2) == 0)
 		{
 			i++;
 			if (filename != NULL)
 				free(filename);
-			if (!(args[i]) || ft_strncmp(args[i], ">", 1) == 0 || ft_strncmp(args[i], "<", 1) == 0)
+			if (!(args[i]))
 			{
 				printf("minishell: syntax error\n");
 				return (NULL);
@@ -68,26 +63,10 @@ char	*get_in_out_file(char **args, char *type1, char *type2)
 	return (filename);
 }
 
-/*
-char	*args_to_line(char **args)
-{
-	char	*line;
-	int		i;
-
-	line = ft_strdup(args[0]);
-	i = 1;
-	while (args[i])
-	{
-		line = ft_strjoin_free(line, " ");
-		line = ft_strjoin_free(line, args[i]);
-		i++;
-	}
-	return (line);
-}
-*/
-
 void	get_in_and_out_file(t_shell *shell, char **args)
 {
 	shell->outfile = get_in_out_file(args, ">", ">>");
 	shell->infile = get_in_out_file(args, "<", "<<");
+
+	printf("%s e %s\n", shell->outfile, shell->infile);
 }
